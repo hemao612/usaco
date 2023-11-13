3c5393ded677dbc87d660a6554c7f54a8606f3e9
Leighton Chen <chenletong19@gmail.com>
2023-11-13 21:36:31 +0800

    commit permutation solution

--- permutation/permutation.cpp
+++ permutation/permutation.cpp
@@ -25,27 +25,31 @@ All the integers of nums are unique.
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void ans(vector<int> perm, vector<int> c, int n){
-   if (perm.size() == n){
-      return;
-   }
-   else{
-      for (int i = 0; i < n ; i++){
-         if (c[i]) continue;
-         c[i] = true;
-         perm.push_back(i);
-         ans(perm, c, n);
-         c[i] = false;
-         perm.pop_back();
-      }
-   }
-   return;
-}
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
 
-int main() {
-   cout<<"================";
-   return 0;
-}
+        _getPermutations(result, nums, 0, nums.size() - 1);
+        return result;
+    }
+
+    void _getPermutations(vector<vector<int>>& result, vector<int> nums, int l, int r){
+        if(l == r){
+            result.push_back(nums);
+            return;
+        } else {
+            for(int i = l; i <= r; i++){
+                swap(nums[l], nums[i]);
+
+                _getPermutations(result, nums, l + 1, r);
+
+                swap(nums[l], nums[i]);
+            }
+        }
+    }
+};
